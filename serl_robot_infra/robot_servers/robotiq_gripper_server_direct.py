b21ce3bbd4a41c2b55190cd63d4abfad20311b0a
JM <jmleeluck@snu.ac.kr>
2024-11-02 14:19:57 +0900

    update robotiq gripper server

---
+++ serl_robot_infra/robot_servers/robotiq_gripper_server_direct.py
@@ -0,0 +1,216 @@
+from robot_servers.gripper_server import GripperServer
+import socket
+import threading
+import time
+from enum import Enum
+from typing import Union, Tuple, OrderedDict
+
+
+class RobotiqGripperServer(GripperServer):
+
+    ACT = 'ACT'  # act : activate (1 while activated, can be reset to clear fault status)
+    GTO = 'GTO'  # gto : go to (will perform go to with the actions set in pos, for, spe)
+    ATR = 'ATR'  # atr : auto-release (emergency slow move)
+    ADR = 'ADR'  # adr : auto-release direction (open(1) or close(0) during auto-release)
+    FOR = 'FOR'  # for : force (0-255)
+    SPE = 'SPE'  # spe : speed (0-255)
+    POS = 'POS'  # pos : position (0-255), 0 = open
+    # READ VARIABLES
+    STA = 'STA'  # status (0 = is reset, 1 = activating, 3 = active)
+    PRE = 'PRE'  # position request (echo of last commanded position)
+    OBJ = 'OBJ'  # object detection (0 = moving, 1 = outer grip, 2 = inner grip, 3 = no object at rest)
+    FLT = 'FLT'  # fault (0=ok, see manual for errors if not zero)
+
+    ENCODING = 'UTF-8'
+
+    class GripperStatus(Enum):
+        """Gripper status reported by the gripper. The integer values have to match what the gripper sends."""
+        RESET = 0
+        ACTIVATING = 1
+        # UNUSED = 2  # This value is currently not used by the gripper firmware
+        ACTIVE = 3
+
+    class ObjectStatus(Enum):
+        """Object status reported by the gripper. The integer values have to match what the gripper sends."""
+        MOVING = 0
+        STOPPED_OUTER_OBJECT = 1
+        STOPPED_INNER_OBJECT = 2
+        AT_DEST = 3
+
+    def __init__(self, gripper_ip):
+        super.__init__()
+        self.socket = None
+        self.command_lock = threading.Lock()
+        self._min_position = 0
+        self._max_position = 255
+        self._min_speed = 0
+        self._max_speed = 255
+        self._min_force = 0
+        self._max_force = 255
+
+    def _connect(self, hostname: str, port: int, socket_timeout: float=2.0) -> None:
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((hostname, port))
+        self.socket.settimeout(socket_timeout)
+    
+    def _disconnect(self) -> None:
+        self.socket.close()
+
+    @staticmethod
+    def _is_ack(data: str):
+        return data == b'ack'
+
+    def _set_vars(self, var_dict : OrderedDict[str, Union[int, float]]) :
+        cmd = "SET"
+        for variable, value in var_dict.item():
+            cmd+=f"{variable} {str(value)}"
+        cmd += "\n"
+
+        with self.command_lock:
+            self.socket.sendall(cmd.encode(self.ENCODING))
+            data = self.socket.recv(1024)
+        return self._is_ack(data)
+    
+    def _set_var(self, variable: str, value: Union[int, float]):
+        return self._set_vars(OrderedDict([(variable, value)]))
+    
+    def _get_var(self, variable: str):
+        with self.command_lock:
+            cmd = f"GET {variable}\n"
+            self.socket.sendall(cmd.encode(self.ENCODING))
+            data = self.socket.recv(1024)
+        var_name, value_str = data.decode(self.ENCODING).split()
+        if var_name != variable:
+            raise ValueError(f"Unexpected response {data} ({data.decode(self.ENCODING)}): does not match '{variable}'")
+        value = int(value_str)
+        return value
+    
+    def _reset(self):
+        self._set_var(self.ACT, 0)
+        self._set_var(self.ATR, 0)
+        while (not self._get_var(self.ACT) == 0 or not self._get_var(self.STA) == 0):
+            self._set_var(self.ACT, 0)
+            self._set_var(self.ATR, 0)
+        time.sleep(0.5)
+    
+    def open(self):
+        self.move_and_wait_for_pos(self.get_open_position(), 64, 1)
+
+    def close(self):
+        self.move_and_wait_for_pos(self.get_closed_position(), 64, 1)
+
+    def move(self, position : int, speed = 255, force = 255) -> Tuple[bool, int]:
+        def clip_val(min_val, val, max_val):
+            return max(min_val, min(val, max_val))
+        
+        clip_pos = clip_val(self._min_position, position, self._max_position)
+        clip_spe = clip_val(self._min_speed, speed, self._max_speed)
+        clip_for = clip_val(self._min_force, force, self._max_force)
+
+        var_dict = OrderedDict([(self.POS, clip_pos), (self.SPE, clip_spe), (self.FOR, clip_for), (self.GTO, 1)])
+        return self._set_vars(var_dict), clip_pos
+
+    def activate_gripper(self, auto_calibrate: bool = True):
+        if not self.is_active():
+            self._reset()
+            while (not self._get_var(self.ACT) == 0 or not self._get_var(self.STA) == 0):
+                time.sleep(0.01)
+
+            self._set_var(self.ACT,1)
+            time.sleep(1.0)
+            while (not self._get_var(self.ACT) == 1 or not self._get_var(self.STA) == 3):
+                time.sleep(0.01)
+
+        if auto_calibrate:
+            self.auto_calibrate()
+
+    def reset_gripper(self):
+        self._reset()
+    
+    def auto_calibrate(self, log: bool = True) -> None:
+        """
+        Attempts to calibrate the open and closed positions, by slowly closing and opening the gripper.
+        :param log: Whether to print the results to log.
+        """
+        # first try to open in case we are holding an object
+        (position, status) = self.move_and_wait_for_pos(self.get_open_position(), 64, 1)
+        if RobotiqGripperServer.ObjectStatus(status) != RobotiqGripperServer.ObjectStatus.AT_DEST:
+            raise RuntimeError(f"Calibration failed opening to start: {str(status)}")
+
+        # try to close as far as possible, and record the number
+        (position, status) = self.move_and_wait_for_pos(self.get_closed_position(), 64, 1)
+        if RobotiqGripperServer.ObjectStatus(status) != RobotiqGripperServer.ObjectStatus.AT_DEST:
+            raise RuntimeError(f"Calibration failed because of an object: {str(status)}")
+        assert position <= self._max_position
+        self._max_position = position
+
+        # try to open as far as possible, and record the number
+        (position, status) = self.move_and_wait_for_pos(self.get_open_position(), 64, 1)
+        if RobotiqGripperServer.ObjectStatus(status) != RobotiqGripperServer.ObjectStatus.AT_DEST:
+            raise RuntimeError(f"Calibration failed because of an object: {str(status)}")
+        assert position >= self._min_position
+        self._min_position = position
+
+        if log:
+            print(f"Gripper auto-calibrated to [{self.get_min_position()}, {self.get_max_position()}]")
+
+    def move_and_wait_for_pos(self, position: int, speed: int, force: int) -> Tuple[int, ObjectStatus]:  # noqa
+        """
+        Sends commands to start moving towards the given position, with the specified speed and force, and
+        then waits for the move to complete.
+        :param position: Position to move to [min_position, max_position]
+        :param speed: Speed to move at [min_speed, max_speed]
+        :param force: Force to use [min_force, max_force]
+        :return: A tuple with an integer representing the last position returned by the gripper after it notified
+        that the move had completed, a status indicating how the move ended (see ObjectStatus enum for details). Note
+        that it is possible that the position was not reached, if an object was detected during motion.
+        """
+        set_ok, cmd_pos = self.move(position, speed, force)
+        if not set_ok:
+            raise RuntimeError("Failed to set variables for move.")
+
+        # wait until the gripper acknowledges that it will try to go to the requested position
+        while self._get_var(self.PRE) != cmd_pos:
+            time.sleep(0.001)
+
+        # wait until not moving
+        cur_obj = self._get_var(self.OBJ)
+        while RobotiqGripperServer.ObjectStatus(cur_obj) == RobotiqGripperServer.ObjectStatus.MOVING:
+            cur_obj = self._get_var(self.OBJ)
+
+        # report the actual position and the object status
+        final_pos = self._get_var(self.POS)
+        final_obj = cur_obj
+        return final_pos, RobotiqGripperServer.ObjectStatus(final_obj)
+
+    def is_active(self):
+        status = self._get_var(self.STA)
+        return RobotiqGripperServer.GripperStatus(status) == RobotiqGripperServer.GripperStatus.ACTIVE
+    
+    def get_min_position(self) -> int:
+        """Returns the minimum position the gripper can reach (open position)."""
+        return self._min_position
+
+    def get_max_position(self) -> int:
+        """Returns the maximum position the gripper can reach (closed position)."""
+        return self._max_position
+
+    def get_open_position(self) -> int:
+        """Returns what is considered the open position for gripper (minimum position value)."""
+        return self.get_min_position()
+
+    def get_closed_position(self) -> int:
+        """Returns what is considered the closed position for gripper (maximum position value)."""
+        return self.get_max_position()
+
+    def is_open(self):
+        """Returns whether the current position is considered as being fully open."""
+        return self.get_current_position() <= self.get_open_position()
+
+    def is_closed(self):
+        """Returns whether the current position is considered as being fully closed."""
+        return self.get_current_position() >= self.get_closed_position()
+
+    def get_current_position(self) -> int:
+        """Returns the current position as returned by the physical hardware."""
+        return self._get_var(self.POS)
