2d3c7a353c4d87b3d10a542767c9e6b0cf114bdc
jmSNU <jmleeluck@snu.ac.kr>
2024-11-15 11:01:50 +0900

    update gripper test

--- serl_robot_infra/robot_servers/gripper_test.py
+++ serl_robot_infra/robot_servers/gripper_test.py
@@ -1,6 +1,6 @@
 import robotiq_gripper_server_direct
 
-ip = "127.0.0.1"
+ip = "192.168.50.146"
 port = 63352
 
 def log_info(gripper):
@@ -9,11 +9,11 @@ def log_info(gripper):
           f"Closed: {gripper.is_closed(): <2}  ")
 
 print("Creating gripper...")
-gripper = robotiq_gripper_server_direct.RobotiqGripper()
+gripper = robotiq_gripper_server_direct.RobotiqGripperServer(gripper_ip = ip)
 print("Connecting to gripper...")
-gripper.connect(ip, port)
+gripper._connect(ip, port)
 print("Activating gripper...")
-gripper.activate()
+gripper.activate_gripper()
 
 print("Testing gripper...")
 gripper.move_and_wait_for_pos(255, 255, 255)
